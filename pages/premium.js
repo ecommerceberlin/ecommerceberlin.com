8eea445515d63e8bbe4d1c04871cd95ad69e2067
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-06-25 22:14:39 +0200

    handle all premium tickets automatically

--- pages/premium.js
+++ pages/premium.js
@@ -3,9 +3,16 @@ import {
   LayoutMain as Layout,
   WidgetTickets,
   Wrapper,
+  TwoColsLayout,
+  DatasourceTickets,
+  Markdown,
+  TicketBuyButton,
 } from 'eventjuicer-site-components';
 
 const settings = require('../settings').default;
+
+const getTicket = (alltickets, ticket) =>
+  alltickets.find(item => item.translation_asset_id.indexOf(ticket) > -1);
 
 class PagePremium extends React.Component {
   static async getInitialProps({ query, asPath, isServer, store }) {
@@ -13,15 +20,55 @@ class PagePremium extends React.Component {
       preload: ['tickets', 'ticketgroups'],
       asPath: asPath,
       settings: settings,
+      ticket: 'slug' in query ? query.slug : null,
     };
   }
 
   render() {
-    const { asPath } = this.props;
+    const { ticket, asPath } = this.props;
 
     return (
       <Layout>
+        <DatasourceTickets>
+          {alltickets => {
+            const matchedTicket = getTicket(alltickets, ticket);
+
+            return matchedTicket ? (
+              <Wrapper
+                first
+                label={`resources.upgrades.misc.${ticket}.name`}
+                secondaryLabel={`resources.upgrades.misc.${ticket}.description`}
+              >
+                <div style={{ marginTop: 80 }}>
+                  <TwoColsLayout
+                    left={
+                      <img
+                        src={matchedTicket.image}
+                        alt=""
+                        style={{ width: '100%' }}
+                      />
+                    }
+                    right={
+                      <div>
+                        <Markdown
+                          label={`resources.upgrades.misc.${ticket}.text`}
+                        />
+                        <TicketBuyButton
+                          id={matchedTicket.id}
+                          bookable={matchedTicket.bookable}
+                          label="common.buy"
+                        />
+                      </div>
+                    }
+                  />
+                </div>
+              </Wrapper>
+            ) : null;
+          }}
+        </DatasourceTickets>
+
         <WidgetTickets
+          first
           label="resources.upgrades.premium.title"
           filter={function(ticket) {
             return ticket.group_id == 305;
