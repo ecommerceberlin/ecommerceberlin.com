57a56a987e8a200b6431835d3985518ea025dab6
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-01 06:31:06 +0200

    max_votes updates

--- pages/vote/[id].js
+++ pages/vote/[id].js
@@ -38,15 +38,15 @@ const PageVote  = ({id}) => (
   <WidgetVotable
       id={id}
       asPath="/vote"
-      vote={<WidgetVoteWithLinkedIn id={id} max_votes={6} />}
-      status={<WidgetVoteStatus total_votes={6} />}
+      vote={<DynamicWidgetVoteWithLinkedIn id={id} max_votes={6} />}
+      status={<WidgetVoteStatus max_votes={6} />}
       show_votes={false}
     />
 
   <WidgetCallForPapers
     intro={
       <div style={{ width: '80%' }}>
-        <WidgetVoteStatus total_votes={6} />
+        <WidgetVoteStatus max_votes={6} />
         <Typography template="benefitsText">
           <Markdown label="callforpapers.voting.general-rules.description" />
         </Typography>
