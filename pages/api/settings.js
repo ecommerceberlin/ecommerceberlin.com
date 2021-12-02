e44fb5cfe5545820e8a38fd6328cc3bfc0a61ede
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-02 22:17:28 +0100

    handle path in external settings fix

--- pages/api/settings.js
+++ pages/api/settings.js
@@ -4,5 +4,5 @@ import {get} from 'eventjuicer-site-components'
 
 export default function handler(req, res) {
     const {path} = req.query
-    res.status(200).json(get(settings, path || ""))
+    res.status(200).json(path? get(settings, path): settings)
 }
