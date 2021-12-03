8267108bd13786f0d99f8b232e599bdf0712ef21
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-03 01:43:35 +0100

    added cors support to api route $5$

--- pages/api/settings.js
+++ pages/api/settings.js
@@ -1,8 +1,9 @@
 
 import settings from '../../settings'
-import {get} from 'eventjuicer-site-components'
+import {get, handleCors} from 'eventjuicer-site-components'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+    await handleCors(req, res)
     const {path} = req.query
     res.status(200).json(path? get(settings, path): settings)
 }
