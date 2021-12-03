5ea75a99a85fd77081f79740ad01e121f8d06b45
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-03 01:03:42 +0100

    we must api path to handle external regs $15$

--- settings/index.js
+++ settings/index.js
@@ -434,6 +434,7 @@ const settings = {
             ticket_id : 1830,
             email_template : "ecommerceberlin-visitor-registration",
             background : "https://res.cloudinary.com/ecommerceberlin/image/upload/c_fit,h_500,w_500/v1546813408/ebe_lanyard1.jpg",
+            api : "https://api.eventjuicer.com/v1/public/hosts/ecommerceberlin.com/register",
 
         },
 
