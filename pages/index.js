e096689e584c802849fac4cb5d809ac778f19b66
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-12-14 12:24:21 +0100

    added new partners widget filtered by "association"
    re https://github.com/ecommerceberlin/ecommerceberlin.com/issues/248

--- pages/index.js
+++ pages/index.js
@@ -72,6 +72,15 @@ const PageIndex = (props) => (
 
 
   <WidgetPartners
+    label="partners.association.title"
+    filter={item =>
+      item['scopes(deprecated)'].indexOf('association') > -1 &&
+      item.logotype.indexOf('cloudinary') > -1
+    }
+    limit={50}
+  />
+
+  <WidgetPartners
     label="partners.media.title"
     filter={item =>
       item['scopes(deprecated)'].indexOf('media') > -1 &&
