a21e6bd4d4e85ff10fa134bd773147f14552fe28
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-01-30 13:05:56 +0100

    WidgetCompanyMeetup regForm options added
    @az tutaj jeszcze konieczna zmiana w WidgetCompanyMeetup?

--- pages/exhibitors/[slug].js
+++ pages/exhibitors/[slug].js
@@ -30,7 +30,34 @@ import { WidgetVisitorNonVip } from '../../compositions';
 
       <HeadCompany slug={slug}>{(data) => <Head>{data}</Head>}</HeadCompany> 
  
-      <WidgetCompanyMeetup />
+      <WidgetCompanyMeetup regFormProps={{
+        options: {
+          participant_type: [
+              "retailer_wholesaler",
+              "brand_manufacturer",
+              "service_provider",
+              "consultant",
+              "developer",
+              "media",
+              "student"
+          ],
+          company_role: [
+              "student",
+              "entry",
+              "manager",
+              "professional",
+              "head_of_department",
+              "director",
+              "c_level",
+              "board_member",
+              "other"
+          ],
+          location: [
+              "yes",
+              "no",
+          ]}
+      }}/>
+      
       <WidgetCompanyVip regFormProps={{
         options: {
           participant_type: [
