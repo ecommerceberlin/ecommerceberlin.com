76d1a79a30213477f45345ab1c1c66b0b31f451d
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-01-07 15:33:12 +0100

    do not show presenters with limited > 0

--- compositions/FilteredPresenters.js
+++ compositions/FilteredPresenters.js
@@ -3,33 +3,8 @@ import { WidgetPresenters } from "eventjuicer-site-components"
 
 const FilteredPresenters = ({limit=100, ...props}) => {
 
-    const excluded_id = [
-        211282,
-        213654,
-        213643,
-        
-        //masterclasses
-        212618,
-        198572,
-        197973,
-        197847,
-        197752,
-        197083,
-        195574,
-        194210,
-        191938,
-        188972,
-        188963,
-        188783,
-        188772,
-        188392,
-        183182,
-        182500
 
-    ];
-
-
-    const filter = (item) => !excluded_id.includes(item.id)
+    const filter = (item) => "limited" in item && !item.limited > 0
 
     return  <WidgetPresenters limit={limit} {...props} filter={filter} />
 
