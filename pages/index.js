3a01a3889b191965af23970a571dcb8937c5e9cb
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-03-05 20:30:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,8 @@ import React from 'react'
 
 import {
   connect,
-  WidgetVideoWithEventInfo,
-  // WidgetVisitor,
-  WidgetFeaturedCompanies,
-  WidgetAllOrCurrentExhibitorsColumnList,
-  WidgetRoleButtons,
   reduxWrapper,
-  configure,
-  // WidgetBanner,
-  WidgetPresenters,
-  WidgetSalesMap,
-  // WidgetSchedule,
-  // WidgetBanner,
-  MyHead,
-  Wrapper,
-  WidgetFeaturedPresenters,
-  Box,
-  MyButton,
 } from 'eventjuicer-site-components';
-
-import {AllPartners, WidgetVisitorNonVip, WidgetVisitorWithVip} from '../compositions'
-import RecapVideo from '../compositions/RecapVideo';
-import Head from 'next/head'
-import WidgetOffer from '../compositions/WidgetOffer';
-
-// import VotingCategories from '../compositions/VotingCategories'
-
-const settings = require('../settings').default;
 
 function PageIndex(){
   return null
